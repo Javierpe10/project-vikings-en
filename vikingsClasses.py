fac11f9745e3b1579abc42655278849a155ae2ee
Javier Peñaloza Tallia <javier.penalozat@gmail.com>
2026-01-24 17:19:38 +0100

    Changes made from David in class

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -5,26 +5,32 @@ import random
 
 class Soldier:
     def __init__(self, health, strength):
-        # your code here
+        self_health = health
+        self_strength = strength
     
     def attack(self):
-        # your code here
+       return self_stength  
 
     def receiveDamage(self, damage):
-        # your code here
+        self_health -= damage 
     
 
 # Viking
 
 class Viking(Soldier):
     def __init__(self, name, health, strength):
-        # your code here
+        super().__init__(self, name, health, strength)
+        self.name = name
 
     def battleCry(self):
-        # your code here
+        return "Odin Owns You All"
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
 
 # Saxon
 
